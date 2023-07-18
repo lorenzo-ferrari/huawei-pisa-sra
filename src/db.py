cfc8025266276835590f6d61991e9109c196deac
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2023-07-18 15:03:46 +0200

    updated db.py

--- src/db.py
+++ src/db.py
@@ -3,26 +3,39 @@ import csv
 
 DB_PATH='resources.db'
 RESOURCES_PATH='../input/resources.csv'
-TABLE_FORMAT = 'id TEXT PRIMARY KEY, name TEXT, location TEXT, model TEXT'
+TABLE_FORMAT = 'id INT PRIMARY KEY, name TEXT, location TEXT, model TEXT'
+TABLE_NAME = 'main_table'
 
 def main():
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
-    create_table_query = f"CREATE TABLE IF NOT EXISTS main_table ({TABLE_FORMAT});"
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} ({TABLE_FORMAT});"
     conn.execute(create_table_query)
 
-    with open(RESOURCES_PATH, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        header = next(csvreader)
-        print(header)
-        insert_query = f"INSERT INTO main_table ({', '.join(header)}) VALUES ({', '.join(['?']*len(header))});"
-        insert_data = [tuple(row) for row in csvreader]
-        print(insert_data)
-        cursor.executemany(insert_query, insert_data)
+    try:
+        with open(RESOURCES_PATH, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            header = next(csvreader)
+            insert_query = f"INSERT INTO {TABLE_NAME} ({', '.join(header)}) VALUES ({', '.join(['?']*len(header))});"
+            insert_data = [tuple(row) for row in csvreader]
+            cursor.executemany(insert_query, insert_data)
+    except:
+        print(f'Something went wrong...')
+        return
 
     conn.commit()
     conn.close()
+
+def print_db():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM {TABLE_NAME}")
+    data = cursor.fetchall()
+    conn.close()
+
+    for row in data:
+        print(row)
 
 if __name__ == '__main__':
     main()
