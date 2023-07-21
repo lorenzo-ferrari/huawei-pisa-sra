873fe23efe10b1ad1fd4c270fb30ac2e5f69b783
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2023-07-21 11:58:53 +0200

    minor changes

--- src/request_from_input.py
+++ src/request_from_input.py
@@ -10,7 +10,7 @@ def main():
     timestamp = int(time.time())
     user_id = input("Enter your user_id: ")
     request_type,value = input("Enter the kind of resource you need (example: 'model TC397XE'): ").split()
-    timeout = int(input("Enter a timeout (in seconds) after which your resource will automatically be freed"))
+    timeout = int(input("Enter a timeout (in seconds) after which your resource will automatically be freed: "))
 
     manager.online_request(timestamp, user_id, request_type, value, timeout)
 
