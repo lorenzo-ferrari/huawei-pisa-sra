331e82b6a04563b18c3e5bd9de6c0b4e09422e6b
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2023-07-21 09:48:27 +0200

    working version. load State object from file

--- src/constants.py
+++ src/constants.py
@@ -1,13 +1,10 @@
 RESOURCES_PATH='../input/resources.csv'
 REQUESTS_PATH='../input/requests.csv'
+DB_PATH='resources.sqlite'
+STATE_PATH = 'state_obj.pickle'
 
-DB_PATH='resources.db'
 TABLE_FORMAT = 'id TEXT PRIMARY KEY, name TEXT, location TEXT, model TEXT, state TEXT'
-TABLE_NAME = 'main_table'
+TABLE_NAME = 'resources_table'
 
 MAX_TIMEOUT = 7200
 ID_NOT_FOUND = -1
-NULL_REQUEST_ID = -1
-
-FREE_EVENT_TYPE = 'FREE'
-REQUEST_EVENT_TYPE = 'REQUEST'
--- src/manager.py
+++ src/manager.py
@@ -3,21 +3,22 @@ import csv
 import heapq
 import logging
 import time
+import pickle
 
 import db
 import constants
 
-logging.basicConfig(filename='../log.txt', filemode='w', format='%(message)s', level=logging.INFO)
+logging.basicConfig(filename='../log.txt', filemode='a', format='%(message)s', level=logging.INFO)
 
 class RequestEvent:
-    def __init__(self, timestamp, request_id, user_id, timeout): # todo, add priorities
+    # in the future, take priorities into account
+    def __init__(self, timestamp, request_id, user_id, timeout):
         self.timestamp = timestamp
         self.request_id = request_id
         self.user_id = user_id
         self.timeout = timeout
 
     def __lt__(self, oth):
-        # in the future, take into account priorities
         return self.timestamp < oth.timestamp
 
 class FreeEvent:
@@ -62,24 +63,34 @@ class State:
         ret = self.cur_request_id
         self.cur_request_id += 1
         return ret
-    
-state = State()
+
+def load_state(filename) -> State:
+    with open(filename, 'rb') as file:
+        ret = pickle.load(file)
+        return ret
+
+def dump_state(filename, state) -> None:
+    with open(filename, 'wb') as file:
+        pickle.dump(state, file, protocol=pickle.HIGHEST_PROTOCOL)
 
 def online_request(timestamp, user_id, request_type, value, timeout = constants.MAX_TIMEOUT) -> None:
     conn = sqlite3.connect(constants.DB_PATH)
-    offline_request(conn, timestamp, user_id, request_type, value, timeout)
+    state = load_state(constants.STATE_PATH)
+    offline_request(conn, state, timestamp, user_id, request_type, value, timeout)
+    dump_state(constants.STATE_PATH, state)
     conn.close()
 
 def online_free(user_id, resource_id) -> None:
     conn = sqlite3.connect(constants.DB_PATH)
-    offline_free(conn, user_id, resource_id)
+    state = load_state(constants.STATE_PATH)
+    offline_free(conn, state, user_id, resource_id)
+    dump_state(constants.STATE_PATH, state)
     conn.close()
 
-def offline_request(conn, timestamp, user_id, request_type, value, timeout) -> None:
-    global state # in the future, load state from file
+def offline_request(conn, state, timestamp, user_id, request_type, value, timeout) -> None:
     state.timer = timestamp
 
-    free_expired_assignments(conn)
+    free_expired_assignments(conn, state)
 
     logging.info(f'[?] Timestamp {state.timer} - user {user_id} requested a resource with "{request_type}":"{value}"')
     request_id = state.new_request_id()
@@ -97,14 +108,13 @@ def offline_request(conn, timestamp, user_id, request_type, value, timeout) -> N
         logging.info(f'[+] Timestamp {state.timer} - request accepted : user {user_id} obtained access to ip {resource_ip} for up to {timeout} seconds')
         state.eventsQ.push(FreeEvent(int(state.timer) + int(timeout), user_id, resource_id))
 
-
-def offline_free(conn, user_id, resource_id) -> None:
+def offline_free(conn, state, user_id, resource_id) -> None:
     db.unlock(conn, int(resource_id))
     logging.info(f'[.] Timestamp {state.timer} - user {user_id} freed the resource with id {resource_id}')
 
-    check_resource_queue(conn, int(resource_id))
+    check_resource_queue(conn, state, int(resource_id))
 
-def check_resource_queue(conn, resource_id):
+def check_resource_queue(conn, state, resource_id):
     # check for users waiting for resource `resource_id`
     while not state.queues[int(resource_id)].empty():
         req = state.queues[int(resource_id)].pop()
@@ -112,13 +122,13 @@ def check_resource_queue(conn, resource_id):
             continue
         else:
             db.lock(conn, int(resource_id))
-            offline_request(conn, state.timer, req.user_id, "id", resource_id, timeout = req.timeout)
+            offline_request(conn, state, state.timer, req.user_id, "id", resource_id, timeout = req.timeout)
             resource_ip = db.ipById(conn, resource_id)
             logging.info(f'[+] Timestamp {state.timer} - user {req.user_id} (queueing) obtained access to ip {resource_ip} for up to {req.timeout} seconds')
             state.eventsQ.push(FreeEvent(int(state.timer) + int(req.timeout), req.user_id, resource_id))
             break
 
-def free_expired_assignments(conn) -> None:
+def free_expired_assignments(conn, state) -> None:
     while not state.eventsQ.empty() and state.eventsQ.top().timestamp <= int(state.timer):
         event = state.eventsQ.pop()
         offline_free(conn, event.user_id, event.resource_id)
@@ -130,15 +140,17 @@ def run_simulation() -> None:
         global state
         for row in csvreader:
             (timestamp, user_id, request_type, value, prio, timeout) = row
-            state.timer = timestamp
             online_request(timestamp, user_id, request_type, value, timeout)
 
-def main():
-    # init manager
+def init_manager():
     conn = sqlite3.connect(constants.DB_PATH)
-    global state
+    state = State()
     state.queues = [CustomQueue() for _ in range(db.cardinality(conn) + 1)]
+    dump_state(constants.STATE_PATH, state)
     conn.close()
+
+def main():
+    init_manager()
 
     run_simulation()
 
